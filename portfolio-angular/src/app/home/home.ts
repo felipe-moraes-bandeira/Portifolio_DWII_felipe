a5ec68c5131843f1e445be38b7054d9c2e7617c5
felipe-moraes <20241ctb0100074@estudantes.ifpr.edu.br>
2026-06-11 20:19:32 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

---
+++ portfolio-angular/src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
